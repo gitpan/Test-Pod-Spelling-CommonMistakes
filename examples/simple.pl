bf9e4b951d4e412de84cd6c93a789d00526b8611
Apocalypse <perl@0ne.us>
2011-02-21 23:39:36 +0000

    Import of APOCAL/Test-Pod-Spelling-CommonMistakes-1.000 from CPAN.
    gitpan-cpan-distribution: Test-Pod-Spelling-CommonMistakes
    gitpan-cpan-version:      1.000
    gitpan-cpan-path:         APOCAL/Test-Pod-Spelling-CommonMistakes-1.000.tar.gz
    gitpan-cpan-author:       APOCAL
    gitpan-cpan-maturity:     released

--- examples/simple.pl
+++ examples/simple.pl
@@ -1,4 +1,12 @@
 #!/usr/bin/perl
+#
+# This file is part of Test-Pod-Spelling-CommonMistakes
+#
+# This software is copyright (c) 2011 by Apocalypse.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
 use strict; use warnings;
 
 use Test::More;
--- lib/Test/Pod/Spelling/CommonMistakes.pm
+++ lib/Test/Pod/Spelling/CommonMistakes.pm
@@ -1,22 +1,48 @@
-# Declare our package
-package Test::Pod::Spelling::CommonMistakes;
+#
+# This file is part of Test-Pod-Spelling-CommonMistakes
+#
+# This software is copyright (c) 2011 by Apocalypse.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
 use strict; use warnings;
+package Test::Pod::Spelling::CommonMistakes;
+BEGIN {
+  $Test::Pod::Spelling::CommonMistakes::VERSION = '1.000';
+}
+BEGIN {
+  $Test::Pod::Spelling::CommonMistakes::AUTHORITY = 'cpan:APOCAL';
+}
 
-# Initialize our version
-use vars qw( $VERSION );
-$VERSION = '0.01';
+# ABSTRACT: Checks POD for common spelling mistakes
 
 # Import the modules we need
-use Pod::Spell::CommonMistakes qw( check_pod_all );
-use Test::Pod ();
+use Pod::Spell::CommonMistakes 0.01 qw( check_pod_all );
+use Test::Pod 1.40 ();
 
 # setup our tests and etc
-use Test::Builder;
+use Test::Builder 0.94;
 my $Test = Test::Builder->new;
 
 # auto-export our 2 subs
 use base qw( Exporter );
 our @EXPORT = qw( pod_file_ok all_pod_files_ok ); ## no critic ( ProhibitAutomaticExportation )
+
+
+sub all_pod_files_ok {
+	my @files = @_ ? @_ : Test::Pod::all_pod_files();
+
+	$Test->plan( tests => scalar @files );
+
+	my $ok = 1;
+	foreach my $file ( @files ) {
+		pod_file_ok( $file ) or undef $ok;
+	}
+
+	return $ok;
+}
+
 
 sub pod_file_ok {
 	my $file = shift;
@@ -53,27 +79,21 @@ sub pod_file_ok {
 	}
 }
 
-sub all_pod_files_ok {
-	my @files = @_ ? @_ : Test::Pod::all_pod_files();
-
-	$Test->plan( tests => scalar @files );
-
-	my $ok = 1;
-	foreach my $file ( @files ) {
-		pod_file_ok( $file ) or undef $ok;
-	}
-
-	return $ok;
-}
-
 1;
-__END__
 
-=for stopwords AnnoCPAN CPAN CPANTS Kwalitee RT TESTNAME spellchecker
+
+__END__
+=pod
+
+=for stopwords TESTNAME spellchecker
 
 =head1 NAME
 
 Test::Pod::Spelling::CommonMistakes - Checks POD for common spelling mistakes
+
+=head1 VERSION
+
+  This document describes v1.000 of Test::Pod::Spelling::CommonMistakes - released February 21, 2011 as part of Test-Pod-Spelling-CommonMistakes.
 
 =head1 SYNOPSIS
 
@@ -91,17 +111,17 @@ Test::Pod::Spelling::CommonMistakes - Checks POD for common spelling mistakes
 
 =head1 DESCRIPTION
 
-This module checks your POD for common spelling errors. This differs than L<Test::Spelling> because it doesn't use your system spellchecker
+This module checks your POD for common spelling errors. This differs from L<Test::Spelling> because it doesn't use your system spellchecker
 and instead uses L<Pod::Spell::CommonMistakes> for the heavy lifting. Using it is the same as any standard Test::* module, as seen here.
 
-=head1 Methods
+=head1 METHODS
 
 =head2 all_pod_files_ok( [ @files ] )
 
 This function is what you will usually run. It automatically finds any POD in your distribution and runs checks on them.
 
 Accepts an optional argument: an array of files to check. By default it checks all POD files it can find in the distribution. Every file it finds
-is passed to the C<pod_file_ok> function.
+is passed to the C<pod_file_ok()> function.
 
 =head2 pod_file_ok( $file, [ $name ] )
 
@@ -119,73 +139,144 @@ Automatically exports the two subs.
 
 =head1 SEE ALSO
 
-L<Pod::Spell::CommonMistakes>
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-	perldoc Test::Pod::Spelling::CommonMistakes
-
-=head2 Websites
+Please see those modules/websites for more information related to this module.
 
 =over 4
 
-=item * Search CPAN
+=item *
 
-L<http://search.cpan.org/dist/Test-Pod-Spelling-CommonMistakes>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Test-Pod-Spelling-CommonMistakes>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Test-Pod-Spelling-CommonMistakes>
-
-=item * CPAN Forum
-
-L<http://cpanforum.com/dist/Test-Pod-Spelling-CommonMistakes>
-
-=item * RT: CPAN's Request Tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-Pod-Spelling-CommonMistakes>
-
-=item * CPANTS Kwalitee
-
-L<http://cpants.perl.org/dist/overview/Test-Pod-Spelling-CommonMistakes>
-
-=item * CPAN Testers Results
-
-L<http://cpantesters.org/distro/T/Test-Pod-Spelling-CommonMistakes.html>
-
-=item * CPAN Testers Matrix
-
-L<http://matrix.cpantesters.org/?dist=Test-Pod-Spelling-CommonMistakes>
-
-=item * Git Source Code Repository
-
-L<http://github.com/apocalypse/perl-test-pod-spelling-commonmistakes>
+L<Pod::Spell::CommonMistakes>
 
 =back
 
-=head2 Bugs
+=for :stopwords cpan testmatrix url annocpan anno bugtracker rt cpants kwalitee diff irc mailto metadata placeholders
 
-Please report any bugs or feature requests to C<bug-test-pod-spelling-commonmistakes at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Pod-Spelling-CommonMistakes>.  I will be
-notified, and then you'll automatically be notified of progress on your bug as I make changes.
+=head1 SUPPORT
+
+=head2 Perldoc
+
+You can find documentation for this module with the perldoc command.
+
+  perldoc Test::Pod::Spelling::CommonMistakes
+
+=head2 Websites
+
+The following websites have more information about this module, and may be of help to you. As always,
+in addition to those websites please use your favorite search engine to discover more resources.
+
+=over 4
+
+=item *
+
+Search CPAN
+
+L<http://search.cpan.org/dist/Test-Pod-Spelling-CommonMistakes>
+
+=item *
+
+RT: CPAN's Bug Tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-Pod-Spelling-CommonMistakes>
+
+=item *
+
+AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test-Pod-Spelling-CommonMistakes>
+
+=item *
+
+CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test-Pod-Spelling-CommonMistakes>
+
+=item *
+
+CPAN Forum
+
+L<http://cpanforum.com/dist/Test-Pod-Spelling-CommonMistakes>
+
+=item *
+
+CPANTS Kwalitee
+
+L<http://cpants.perl.org/dist/overview/Test-Pod-Spelling-CommonMistakes>
+
+=item *
+
+CPAN Testers Results
+
+L<http://cpantesters.org/distro/T/Test-Pod-Spelling-CommonMistakes.html>
+
+=item *
+
+CPAN Testers Matrix
+
+L<http://matrix.cpantesters.org/?dist=Test-Pod-Spelling-CommonMistakes>
+
+=back
+
+=head2 Email
+
+You can email the author of this module at C<APOCAL at cpan.org> asking for help with any problems you have.
+
+=head2 Internet Relay Chat
+
+You can get live help by using IRC ( Internet Relay Chat ). If you don't know what IRC is,
+please read this excellent guide: L<http://en.wikipedia.org/wiki/Internet_Relay_Chat>. Please
+be courteous and patient when talking to us, as we might be busy or sleeping! You can join
+those networks/channels and get help:
+
+=over 4
+
+=item *
+
+irc.perl.org
+
+You can connect to the server at 'irc.perl.org' and join this channel: #perl-help then talk to this person for help: Apocalypse.
+
+=item *
+
+irc.freenode.net
+
+You can connect to the server at 'irc.freenode.net' and join this channel: #perl then talk to this person for help: Apocal.
+
+=item *
+
+irc.efnet.org
+
+You can connect to the server at 'irc.efnet.org' and join this channel: #perl then talk to this person for help: Ap0cal.
+
+=back
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests by email to C<bug-test-pod-spelling-commonmistakes at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Pod-Spelling-CommonMistakes>. You will be automatically notified of any
+progress on the request by the system.
+
+=head2 Source Code
+
+The code is open to the world, and available for you to hack on. Please feel free to browse it and play
+with it, or whatever. If you want to contribute patches, please send me a diff or prod me to pull
+from your repository :)
+
+L<http://github.com/apocalypse/perl-test-pod-spelling-commonmistakes>
+
+  git clone git://github.com/apocalypse/perl-test-pod-spelling-commonmistakes.git
 
 =head1 AUTHOR
 
-Apocalypse E<lt>apocal@cpan.orgE<gt>
+Apocalypse <APOCAL@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2010 by Apocalypse
+This software is copyright (c) 2011 by Apocalypse.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
-The full text of the license can be found in the LICENSE file included with this module.
+The full text of the license can be found in the LICENSE file included with this distribution.
 
 =cut
+
